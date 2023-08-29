02546292bf4a2054988ae6604725147312644a48
liangyalong1314 <503934353@qq.com>
2023-08-29 21:44:32 +0800

    2
    2

--- app/src/main/java/com/lyl/face/MainActivity.kt
+++ app/src/main/java/com/lyl/face/MainActivity.kt
@@ -17,9 +17,7 @@ import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
 import com.google.common.util.concurrent.ListenableFuture
 import com.lyl.face.databinding.ActivityMainBinding
-import com.lyl.face.demo.ProcessCamera2Provider
-import com.lyl.face.demo.ProcessCameraProvider
-import com.lyl.tencent_face.bean.Frame
+
 import com.lyl.tencent_face.manager.FaceManager
 import com.lyl.tencent_face.utils.ImageConverter
 import java.nio.ByteBuffer
@@ -36,71 +34,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        FileUtil.init(this)
-        var authWithDeviceSn =
-            face.initFace(this, "p8rzuoxtj", "4b9da57aae624850b2d2c20b11f476ae", FileUtil . getSdCardCacheDir ()!!.absolutePath
-                .toString() + "/fubaoFace/")
-        Log.i("TAG", "onCreate: "+authWithDeviceSn)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        //TODO 省略了权限申请，具体看文章中 "前置操作" 部分
-
-
-        imageAnalysis.setAnalyzer(Executors.newSingleThreadExecutor(), ImageAnalysis.Analyzer { imageProxy ->
-            var toBitmap = ImageConverter.toBitmap(imageProxy.image!!)
-            var frame =
-                Frame(ImageConverter.bitmap2RGB(toBitmap), toBitmap.width, toBitmap.height, 1)
-            Log.i("TAG", "onCreate: "+face.findFace(frame,true))
-            imageProxy.close()
-        })
-        setUpCamera(binding.previewView2)
-    }
-
-    private fun setUpCamera(previewView: PreviewView) {
-        val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> =
-            ProcessCameraProvider.getInstance(this)
-        cameraProviderFuture.addListener({
-            try {
-               var cameraProvider = cameraProviderFuture.get()
-                preview = Preview.Builder().build()
-                var cameraSelector : CameraSelector = CameraSelector.Builder()
-                    .addCameraFilter(MyCameraFilter(0))
-                    .build()
-                preview?.setSurfaceProvider(binding.previewView.surfaceProvider)
-                cameraProvider.bindToLifecycle(
-                    this,
-                    cameraSelector, preview
-                )
-
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }, ContextCompat.getMainExecutor(this))
-
-
-
-        val cameraProviderFuture2: ListenableFuture<ProcessCamera2Provider> =
-            ProcessCamera2Provider.getInstance(this)
-        cameraProviderFuture2.addListener({
-            try {
-                var cameraProvider = cameraProviderFuture2.get()
-                preview = Preview.Builder().build()
-                var cameraSelector : CameraSelector = CameraSelector.Builder()
-                    .addCameraFilter(MyCameraFilter(1))
-                    .build()
-               preview?.setSurfaceProvider(binding.previewView2.surfaceProvider)
-                cameraProvider.bindToLifecycle(
-                    this,
-                    cameraSelector, preview,imageAnalysis
-                )
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }, ContextCompat.getMainExecutor(this))
 
     }
-
 
 
     val imageAnalysis = ImageAnalysis.Builder()
